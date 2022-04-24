65eae93f40265ec34772711e89b7243a275b4bd7
Efjulia <efjulia@mail.ru>
2022-04-24 22:11:16 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp4/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+namespace WinFormsApp4
+{
+        class SQLClass
+        {
+            public static MySqlConnection conn;
+            public static List<string> Select(string Text)
+            {
+
+
+
+                List<string> results = new List<string>();
+
+                MySqlCommand command = new MySqlCommand(Text, conn);
+                DbDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        results.Add(reader.GetString(i));
+                }
+
+                reader.Close();
+                command.Dispose();
+
+                return results;
+            }
+
+            public static void Insert(string Text)
+            {
+                MySqlCommand command = new MySqlCommand(Text, conn);
+                DbDataReader reader = command.ExecuteReader();
+                reader.Close();
+                command.Dispose();
+            }
+        }
+    }
+
+
